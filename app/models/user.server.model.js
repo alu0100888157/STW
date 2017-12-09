d7248bcf4af74fbc041be877af05d1a3de077077
Javier <alu0100819441@ull.edu.es>
2017-12-09 17:14:41 +0000

    Add default created value to UserSchema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,7 +5,16 @@ const UserSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
-    password: String
+    username: {
+        type: String,
+        // predifined modifiers
+        trim: true // remove whitespaces
+    },
+    password: String,
+    // default values
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 mongoose.model('User', UserSchema);
