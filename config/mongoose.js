e20834fb4721eeadc528f40d2926df3eb305087e
Javier <alu0100819441@ull.edu.es>
2017-12-09 12:26:32 +0000

    Register the user model

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports = function() {
     const db = mongoose.connect(config.db);
-    
+    // Register the user model
+    require('../app/models/user.server.model');
     return db;
 };
