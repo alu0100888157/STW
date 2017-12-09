4ddef3ab075061b503f9fa9b23c015719f6d9dd4
Javier <alu0100819441@ull.edu.es>
2017-12-09 13:53:02 +0000

    Define and route list controller method

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,6 +1,6 @@
 // Users controller to handle all user-ralated operations.
 const User = require('mongoose').model('User');
-
+// create users
 exports.create = function(req, res, next) {
     const user = new User(req.body);
 
@@ -9,6 +9,16 @@ exports.create = function(req, res, next) {
             return next(err);
         } else {
             res.status(200).json(user);
+        }
+    });
+};
+// find multiple user documents
+exports.list = function(req, res, next) {
+    User.find({}, (err, users) => {
+        if (err) {
+            return next(err);
+        } else {
+            res.status(200).json(users);
         }
     });
 };
