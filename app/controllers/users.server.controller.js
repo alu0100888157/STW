f4337ca25d82f1eca66d80111cd5f5680fb97713
Javier <alu0100819441@ull.edu.es>
2017-12-09 18:34:28 +0000

    Update the UserSchema.
    - Add methods to allow users sign in, sign up and log out.

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,5 +1,6 @@
 // Users controller to handle all user-ralated operations.
 const User = require('mongoose').model('User');
+const passport = require('passport');
 // create users
 exports.create = function(req, res, next) {
     const user = new User(req.body);
@@ -67,4 +68,73 @@ exports.delete = function(req, res, next) {
             res.status(200).json(req.user);
         }
     })
+};
+
+function getErrorMessage(err) {
+    let message = '';
+
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong';
+        }
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) message = err.errors[errName].message;
+        }
+    }
+    return message;
+};
+// Controller method to render the sign-in page
+exports.renderSignin = function(req, res, next) {
+    if (!req.user) {
+        res.render('signin', {
+            title: 'Sign-in Form',
+            messages: req.flash('error') || req.flash('info')
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+// Controller methdo to render the signup page
+exports.renderSignup = function(req, res, next) {
+    if (!req.user) {
+        res.render('signup', {
+            title: 'Sign-up Form',
+            messages: req.flash('error')
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.signup = function(req, res, next) {
+    if (!req.user) {
+        const user = new User(req.body);
+        user.provider = 'local';
+
+        user.save((err) => {
+            if (err) {
+                const message = getErrorMessage(err);
+
+                req.flash('error', message);
+                return res.redirect('/signup');
+            }
+            req.login(user, (err) => {
+                if (err) return next(err);
+                return res.redirect('/');
+            });
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.signout = function(req, res) {
+    req.logout();
+    res.redirect('/');
 };
