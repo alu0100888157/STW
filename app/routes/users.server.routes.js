d937b50b877809d343091f5bd19d116a932d9029
Javier <alu0100819441@ull.edu.es>
2017-12-09 22:56:07 +0000

    Install and configure Passport's Google strategy

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -39,6 +39,20 @@ module.exports = function(app) {
         failureRedirect: '/signin',
         successRedirect: '/'
     }));
+
+    // WIRING PASSPORT'S GOOGLE STRATEGY ROUTES
+    app.get('/oauth/google', passport.authenticate('google', {
+        failureRedirect: '/signin',
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ],
+    }));
+
+    app.get('/oauth/google/callback', passport.authenticate('google', {
+        failureRedirect: '/signin',
+        successRedirect: '/'
+    }));
     
     app.route('/users')
     .post(users.create)
--- config/env/development.js
+++ config/env/development.js
@@ -15,5 +15,10 @@ module.exports = {
         clientID: 'Application Id',
         clientSecret: 'Application Secret',
         callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+    },
+    google: {
+        clientID: 'Application Id',
+        clientSecret: 'Application Secret',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
     }
 };
