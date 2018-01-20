71dba10b97d926809a261834f6660c1383ec19d2
Javier <alu0100819441@ull.edu.es>
2018-01-20 23:11:43 +0100

    Implement error handling method

--- app/controllers/articles.server.controller.js
+++ app/controllers/articles.server.controller.js
@@ -1,3 +1,14 @@
 // Load the module dependencies
 const mongoose = require('mongoose');
 const Article = mongoose.model('Article');
+
+// Create a new error handling controller method
+const getErrorMessage = function(err) {
+    if (err.errors) {
+        for (const errName in err.errors) {
+            if (err.errors[errName].message) return err.errors[errName].message;
+        }
+    } else {
+        return 'Unknown server error';
+    }
+};
