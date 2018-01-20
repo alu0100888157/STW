ac70a640c0d23e2e90d906259f9ba4b6da3c07af
Javier <alu0100819441@ull.edu.es>
2018-01-20 18:17:17 +0100

    Modify the users' server controller.
    To support our authentication refactoring, we make sure our user's server controller is able to process the Angular service requests.
    
    users.server.controller.js file:
    
    - Encapsulate the authentication logic inside two methods that can accept and respond with a JSON object.
    
    users.server.routes.js file:
    
    - Remove the routes that it has been used to render our authentication views.
    
    - Add /api prefix for all the routes. It's a very good practice to keep all your rourtes under one prefix, since we want the Angular router to be able to have routes that do not interfere with our server routes.

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,80 +1,6 @@
 // Users controller to handle all user-ralated operations.
 const User = require('mongoose').model('User');
 const passport = require('passport');
-// create users
-exports.create = function(req, res, next) {
-    const user = new User(req.body);
-
-    user.save((err) => {
-        if (err) {
-            return next(err);
-        } else {
-            res.status(200).json(user);
-        }
-    });
-};
-// find multiple user documents
-exports.list = function(req, res, next) {
-    User.find({}, 'username email', {
-        limit: 10
-    }, (err, users) => {
-        if (err) {
-            return next(err);
-        } else {
-            res.status(200).json(users);
-        }
-    });
-};
-
-/* read method responds with a JSON representation of the req.user object, but
-what is creating the req.user object? the userById() method is the one responsible
-for populating the req.user object. You will use the userById() method as a 
-middleware to deal with the manipulation of single documents when performing read,
-delete, and update operations.
-*/
-exports.read = function(req, res) {
-    res.json(req.user);
-};
-
-// reading a single user document using findOne()
-// userByID method. Middleware to deal with the manipulation
-// of single documents when performing CRUD operations.
-exports.userByID = function(req, res, next, id) {
-    User.findOne({
-        _id: id
-    }, (err, user) => {
-        if (err) {
-            return next(err);
-        } else {
-            req.user = user;
-            next();
-        }
-    });
-};
-
-// Update an existing user document
-exports.update = function(req, res, next) {
-    User.findByIdAndUpdate(req.user.id, req.body, {
-        'new':true
-    }, (err, user) => {
-        if(err) {
-            return next(err);
-        } else {
-            res.status(200).json(user);
-        }
-    });
-};
-
-// Delete an existing user document
-exports.delete = function(req, res, next) {
-    req.user.remove(err => {
-        if (err) {
-            return next(err);
-        } else {
-            res.status(200).json(req.user);
-        }
-    })
-};
 
 function getErrorMessage(err) {
     let message = '';
@@ -95,27 +21,25 @@ function getErrorMessage(err) {
     }
     return message;
 };
-// Controller method to render the sign-in page
-exports.renderSignin = function(req, res, next) {
-    if (!req.user) {
-        res.render('signin', {
-            title: 'Sign-in Form',
-            messages: req.flash('error') || req.flash('info')
-        });
-    } else {
-        return res.redirect('/');
-    }
-};
-// Controller methdo to render the signup page
-exports.renderSignup = function(req, res, next) {
-    if (!req.user) {
-        res.render('signup', {
-            title: 'Sign-up Form',
-            messages: req.flash('error')
-        });
-    } else {
-        return res.redirect('/');
-    }
+
+exports.signin = function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err || !user) {
+            res.status(400).send(info);
+        } else {
+            // Remove sensitive data before login
+            user.password = undefined;
+            user.salt = undefined;
+
+            req.login(user, function(err) {
+                if (err) {
+                    res.status(400).send(err);
+                } else {
+                    res.json(user);
+                }
+            });
+        }
+    }) (req, res, next);
 };
 
 exports.signup = function(req, res, next) {
@@ -125,19 +49,23 @@ exports.signup = function(req, res, next) {
 
         user.save((err) => {
             if (err) {
-                const message = getErrorMessage(err);
-                // read messages written to the flash
-                req.flash('error', message);
-                return res.redirect('/signup');
+                return res.status(400).send({
+                    message: getErrorMessage(err)
+                });
+            } else {
+                // Remove sensitive data before login
+                user.password = undefined;
+                user.salt = undefined;
+
+                req.login(user, function(err) {
+                    if (err) {
+                        res.status(400).send(err);
+                    } else {
+                        res.json(user);
+                    }
+                });
             }
-            // create user session
-            req.login(user, (err) => {
-                if (err) return next(err);
-                return res.redirect('/');
-            });
         });
-    } else {
-        return res.redirect('/');
     }
 };
 
@@ -158,11 +86,16 @@ exports.saveOAuthUserProfile = function(req, profile, done) {
             if (!user) {
                 const possibleUsername = profile.username || ((profile.email) ? profile.email.split('@')[0] : '');
 
-                User.findUniqueUsername(possibleUsername, null, (availableUsername) => {
-                    const newUser = new User(profile);
-                    newUser.username = availableUsername;
+                User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
+                    profile.username = availableUsername;
+                    user = new User(profile);
 
-                    newUser.save((err) => {
+                    user.save((err) => {
+                        if (err) {
+                            const message = _this.getErrorMessage(err);
+                            req.flash('error', message);
+                            return res.redirect('/signup');
+                        }
                         return done(err, newUser);
                     });
                 });
@@ -172,3 +105,104 @@ exports.saveOAuthUserProfile = function(req, profile, done) {
         }
     });
 };
+
+// From this on, these functions are no longer necessary because authentication
+// refactoring to process the Angular service requests, but the have been left as 
+// a reference. 
+// create users
+// exports.create = function(req, res, next) {
+//     const user = new User(req.body);
+
+//     user.save((err) => {
+//         if (err) {
+//             return next(err);
+//         } else {
+//             res.status(200).json(user);
+//         }
+//     });
+// };
+// // find multiple user documents
+// exports.list = function(req, res, next) {
+//     User.find({}, 'username email', {
+//         limit: 10
+//     }, (err, users) => {
+//         if (err) {
+//             return next(err);
+//         } else {
+//             res.status(200).json(users);
+//         }
+//     });
+// };
+
+// /* read method responds with a JSON representation of the req.user object, but
+// what is creating the req.user object? the userById() method is the one responsible
+// for populating the req.user object. You will use the userById() method as a 
+// middleware to deal with the manipulation of single documents when performing read,
+// delete, and update operations.
+// */
+// exports.read = function(req, res) {
+//     res.json(req.user);
+// };
+
+// // reading a single user document using findOne()
+// // userByID method. Middleware to deal with the manipulation
+// // of single documents when performing CRUD operations.
+// exports.userByID = function(req, res, next, id) {
+//     User.findOne({
+//         _id: id
+//     }, (err, user) => {
+//         if (err) {
+//             return next(err);
+//         } else {
+//             req.user = user;
+//             next();
+//         }
+//     });
+// };
+
+// // Update an existing user document
+// exports.update = function(req, res, next) {
+//     User.findByIdAndUpdate(req.user.id, req.body, {
+//         'new':true
+//     }, (err, user) => {
+//         if(err) {
+//             return next(err);
+//         } else {
+//             res.status(200).json(user);
+//         }
+//     });
+// };
+
+// // Delete an existing user document
+// exports.delete = function(req, res, next) {
+//     req.user.remove(err => {
+//         if (err) {
+//             return next(err);
+//         } else {
+//             res.status(200).json(req.user);
+//         }
+//     })
+// };
+// Controller method to render the sign-in page
+// exports.renderSignin = function(req, res, next) {
+//     if (!req.user) {
+//         res.render('signin', {
+//             title: 'Sign-in Form',
+//             messages: req.flash('error') || req.flash('info')
+//         });
+//     } else {
+//         return res.redirect('/');
+//     }
+// };
+
+// Controller methdo to render the signup page
+// exports.renderSignup = function(req, res, next) {
+//     if (!req.user) {
+//         res.render('signup', {
+//             title: 'Sign-up Form',
+//             messages: req.flash('error')
+//         });
+//     } else {
+//         return res.redirect('/');
+//     }
+// };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -3,46 +3,35 @@ const users = require('../../app/controllers/users.server.controller');
 const passport = require('passport');
 
 module.exports = function(app) {
-    // wiring the user's routes
-    app.route('/signup')
-    .get(users.renderSignup)
-    .post(users.signup);
-    
-    app.route('/signin')
-    .get(users.renderSignin)
-    .post(passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/signin',
-        failureFlash:true
-    }));
-
-    app.get('/signout', users.signout);
+    app.route('/api/auth/signup').post(users.signup);
+    app.route('/api/auth/signin').post(users.signin);
+    app.route('/api/auth/signout').get(users.signout);
 
     // WIRING PASSPOR'S FACEBOOK STRATEGY ROUTES ******************************************
     // start the user authentication process.
-    app.get('/oauth/facebook', passport.authenticate('facebook', {
+    app.get('/api/oauth/facebook', passport.authenticate('facebook', {
         failureRedirect: '/signin'
     }));
     // finish the authentication process once the user has linked their
     // facebook profile.
-    app.get('/oauth/facebook/callback', passport.authenticate('facebook',
+    app.get('/api/oauth/facebook/callback', passport.authenticate('facebook',
     {
         failureRedirect: '/signin',
         successRedirect: '/'
     }));
 
     // WIRING PASSPORT'S TWITTER STRATEGY ROUTES
-    app.get('/oauth/twitter', passport.authenticate('twitter', {
+    app.get('/api/oauth/twitter', passport.authenticate('twitter', {
         failureRedirect: '/signin'
     }));
 
-    app.get('/oauth/twitter/callback', passport.authenticate('twitter', {
+    app.get('/api/oauth/twitter/callback', passport.authenticate('twitter', {
         failureRedirect: '/signin',
         successRedirect: '/'
     }));
 
     // WIRING PASSPORT'S GOOGLE STRATEGY ROUTES
-    app.get('/oauth/google', passport.authenticate('google', {
+    app.get('/api/oauth/google', passport.authenticate('google', {
         failureRedirect: '/signin',
         scope: [
             'https://www.googleapis.com/auth/userinfo.profile',
@@ -50,26 +39,8 @@ module.exports = function(app) {
         ],
     }));
 
-    app.get('/oauth/google/callback', passport.authenticate('google', {
+    app.get('/api/oauth/google/callback', passport.authenticate('google', {
         failureRedirect: '/signin',
         successRedirect: '/'
     }));
-    
-    // app.route('/users')
-    // .post(users.create)
-    // .get(users.list);
-    
-    /*
-        In Express, adding a colon before a substring in a route definition means
-        that this substring will be handled as a request parameter. To handle the
-        population of the req.user object, you use the app.param() method, which
-        defines a middleware to be executed before any other middleware that uses
-        that parameter.
-    */
-    // app.route('/users/:userId')
-    // .get(users.read)
-    // .put(users.update)
-    // .delete(users.delete);
-
-    // app.param('userId', users.userByID);
 };
