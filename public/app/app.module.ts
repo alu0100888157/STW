769287efe374f77b05a837d8c5f821f621de3a48
Javier <alu0100819441@ull.edu.es>
2018-01-20 19:06:08 +0100

    Refactior the application module.
    - Import HTTP module.
    
    - Import new home and authentication modules along with our new Application routing configuration.
    
    - Inject the authentication service in the providers property so that it is available for all of our submodules.
    
    - Implement our application routing configuration.

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -4,15 +4,28 @@ component and uses it for bootstrapping.
 */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routes';
+// Include our authentication and home component.
+import { HomeModule } from './home/home.module';
+import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        HttpModule,
+        AuthenticationModule,
+        HomeModule,
+        RouterModule.forRoot(AppRoutes),
     ],
     declarations: [
         AppComponent
+    ],
+    providers: [
+        AuthenticationService
     ],
     bootstrap: [AppComponent]
 })
